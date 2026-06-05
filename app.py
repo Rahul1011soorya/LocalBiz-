fb475b37fde4648f36733730e17ebacb79153c76
Rahul1011soorya <anands1011ad@gmail.com>
2026-06-05 13:40:49 +0530

    add about changes

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(
+    __name__,
+    template_folder="frontend",
+    static_folder="frontend",
+    static_url_path=""
+)
+
+
+@app.route("/")
+@app.route("/index.html")
+def home():
+    return render_template("index.html")
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000,debug=True)
